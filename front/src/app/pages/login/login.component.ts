8599a0328a5b20368312d94d63f08957c202d3c2
Guilherme Henrique Pinheiro da Silva <guilhermepinheirohs@gmail.com>
2025-03-20 11:31:21 -0300

    login

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
