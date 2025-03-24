419b811ff4269bb925c7ae31f2cf1989f33cca0e
GUILHERME  HENRIQUE PINHEIRO DA SILVA <0221432411021@fatectq.local>
2025-03-24 12:30:28 -0300

    productPage

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent
+  ],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
