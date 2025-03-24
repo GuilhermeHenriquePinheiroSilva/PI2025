5cc43d102c39d61c8ce6c9983caf9d30bc65fab7
LUCAS  HENRIQUE DOS SANTOS <0221432411009@fatectq.local>
2025-03-24 10:21:07 -0300

    update css

---
+++ src/app/pages/forgot-pass/forgot-pass.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-pass',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './forgot-pass.component.html',
+  styleUrl: './forgot-pass.component.css'
+})
+export class ForgotPassComponent {
+
+}
