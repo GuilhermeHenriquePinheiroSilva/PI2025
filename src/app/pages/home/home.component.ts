890ce41da87e400384032d62da3e4e2220e77268
LUCAS  HENRIQUE DOS SANTOS <0221432411009@fatectq.local>
2025-03-31 10:57:47 -0300

    img

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink,
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
