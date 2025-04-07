a190e0af1e76f313b8f8cce17f745ba4f4eb8c6c
LUCAS  HENRIQUE DOS SANTOS <0221432411009@fatectq.local>
2025-04-07 11:17:17 -0300

    uppdate

--- auth.py
+++ auth.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 def create_access_token(data: dict):
     to_encode = data.copy()
